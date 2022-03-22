b3902dfda58a730f5b3bec85b5476e317ea7ebad
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2022-03-22 23:24:41 +0100

    d

--- ansible/entrypoint.sh
+++ ansible/entrypoint.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-ansible-playbook -i inventories/inventory.yml roles/all.yml --tags all --limit all --connection=local
-
-ls -lh /tmp
+ansible-playbook -i inventories/inventory.yml roles/all.yml --tags all --limit all
